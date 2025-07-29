06ab177e76206a7128a790b5c352caaa5e6559ee
Gabriel <yoalgrin@hotmail.com>
2025-07-29 15:30:16 +0200

    S001 complet

---
+++ server.js
@@ -0,0 +1,23 @@
+// Charge les variables d'environnement
+require("dotenv").config();
+
+// Importe express
+const express = require("express");
+const app = express();
+
+// Configure le moteur de vues EJS
+app.set("view engine", "ejs");
+app.set("views", "./views"); // Dossier racine "views"
+
+// Route GET /
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Récupère le port depuis .env
+const port = process.env.PORT_NO || 3000;
+
+// Lance le serveur
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
